e5e1f73907cf1719823e6acdfa3997627ff89613
alex <admin@stockholdergame.com>
2021-11-20 15:18:44 +0100

    fix 2

--- src/scripts/notificationHandler.js
+++ src/scripts/notificationHandler.js
@@ -17,9 +17,11 @@ export function handleNotifications(notifications) {
     let notifReqRefresh = false
     let notifGameIds = []
     for (const notification of notifications) {
-        notifReqRefresh = notification.type === 'GAME_STARTED' || notification.type === 'MOVE_DONE'
-            || notification.type === 'GAME_FINISHED'
-        notifGameIds.push(notification.body.gameId)
+        if (notification.type === 'GAME_STARTED' || notification.type === 'MOVE_DONE'
+            || notification.type === 'GAME_FINISHED') {
+            notifReqRefresh = true
+            notifGameIds.push(notification.body.gameId)
+        }
     }
     for (const page of refreshablePages) {
         if (_currentPath.includes(page)) {
