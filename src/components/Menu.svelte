1d87e1969d2a60496c5e7b993b1841d6f7485423
alex <admin@stockholdergame.com>
2021-12-18 16:31:44 +0100

    responsive menu

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -14,7 +14,7 @@
     }
 </script>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-default navbar-fixed-top navbar-expand-lg navbar-light bg-light">
     {#if $authenticated}
         <a class="navbar-brand" href="/secure/home">
             <img src="/logo-15-small.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
@@ -27,36 +27,44 @@
         </a>
     {/if}
 
-    {#if $authenticated}
-        <ul class="nav navbar-nav">
-            <li><a class="nav-link" href="/secure/newgame">Новая Игра</a></li>
-            <li><a class="nav-link" href="/secure/gamesarchive">Архив Игр</a></li>
-            <li><a class="nav-link" href="/secure/players">Игроки</a></li>
-        </ul>
-    {/if}
-
-    <ul class="nav navbar-nav">
-        <li><a class="nav-link" href="/about">Об Игре</a></li>
-        <li><a class="nav-link" href="https://shareholdergame.com/rules/index.html">Правила</a></li>
-        <li><a class="nav-link" href="https://forum.stockholdergame.com">Форум</a></li>
-    </ul>
-
-    <ul class="nav navbar-nav">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
         {#if $authenticated}
-            <li>
-                <button class="btn btn-outline-secondary" on:click={onClickChatBtn}>
-                    <i class="bi bi-chat-dots"></i>
-                    Chat
-                    {#if $newChatMessages.length > 0}
-                        <span class="badge badge-danger">{$newChatMessages.length}</span>
-                    {/if}
-                </button>
-            </li>
-            <li><a class="nav-link" href="/secure/profile">{$user}</a></li>
-            <li><button class="btn btn-outline-secondary" on:click={onSignOut}>Выйти</button></li>
-        {:else}
-            <li><a class="nav-link" href="/signin">Войти</a></li>
-            <li><a class="nav-link" href="/signup">Регистрация</a></li>
+            <ul class="nav navbar-nav">
+                <li><a class="nav-link" href="/secure/newgame">Новая Игра</a></li>
+                <li><a class="nav-link" href="/secure/gamesarchive">Архив Игр</a></li>
+                <li><a class="nav-link" href="/secure/players">Игроки</a></li>
+            </ul>
         {/if}
-    </ul>
+
+        <ul class="nav navbar-nav">
+            <li><a class="nav-link" href="/about">Об Игре</a></li>
+            <li><a class="nav-link" href="https://shareholdergame.com/rules/index.html">Правила</a></li>
+            <li><a class="nav-link" href="https://forum.stockholdergame.com">Форум</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav">
+            {#if $authenticated}
+                <li>
+                    <button class="btn btn-outline-secondary" on:click={onClickChatBtn}>
+                        <i class="bi bi-chat-dots"></i>
+                        Chat
+                        {#if $newChatMessages.length > 0}
+                            <span class="badge badge-danger">{$newChatMessages.length}</span>
+                        {/if}
+                    </button>
+                </li>
+                <li><a class="nav-link" href="/secure/profile">{$user}</a></li>
+                <li>
+                    <button class="btn btn-outline-secondary" on:click={onSignOut}>Выйти</button>
+                </li>
+            {:else}
+                <li><a class="nav-link" href="/signin">Войти</a></li>
+                <li><a class="nav-link" href="/signup">Регистрация</a></li>
+            {/if}
+        </ul>
+    </div>
 </nav>
