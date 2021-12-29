e2e6985783339750cbd3a4ee57cd6f3499e0a8de
alex <admin@stockholdergame.com>
2021-12-29 16:02:45 +0100

    improve script

--- pack.sh
+++ pack.sh
@@ -2,13 +2,15 @@
 
 FILE_NAME="site.tgz"
 
-#sed -i '' 's/http/https/g' src/scripts/apiUrl.js
-#sed -i '' 's/localhost/stockholdergame.com/g' src/scripts/apiUrl.js
+sed -i '' 's/http/https/g' src/scripts/apiUrl.js
+sed -i '' 's/localhost/stockholdergame.com/g' src/scripts/apiUrl.js
 
-#npm run build
+npm run build
 
 if [ -f $FILE_NAME ]; then
   rm $FILE_NAME
 fi
 tar -czf $FILE_NAME public
 ls -l |grep $FILE_NAME
+
+git checkout HEAD -- src/scripts/apiUrl.js
