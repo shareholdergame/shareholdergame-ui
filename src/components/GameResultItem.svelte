935c832a7239f33ffc2f48824a6ef661f482f616
alex <admin@stockholdergame.com>
2022-01-08 09:09:42 +0100

    fix page

--- src/components/GameResultItem.svelte
+++ src/components/GameResultItem.svelte
@@ -9,35 +9,41 @@
 
 <div class="card mb-1">
     <div class="card-body d-flex flex-row">
-        <div class="flex-column text-center">
-            <b>{gameSet.options.cardOption.major}&nbsp;x&nbsp;{gameSet.options.cardOption.minor}</b>
-            <small>
-                {gameSet.finishedTime}
-            </small>
+        <div class="d-flex flex-column text-center">
+            <div>
+                <b>{gameSet.options.cardOption.major}&nbsp;x&nbsp;{gameSet.options.cardOption.minor}</b>
+            </div>
+            <div>
+                <small>
+                    {gameSet.finishedTime}
+                </small>
+            </div>
         </div>
-        <table class="table table-borderless table-sm">
-            <tr>
-                {#each gameSet.result as result}
-                    <td class="text-center">
+        <div class="flex-fill">
+            <table class="table table-borderless table-sm">
+                <tr>
+                    {#each gameSet.result as result}
+                        <td class="text-center">
                         <span class="font-weight-bold ml-2">
                             <PlayerNameLink name="{result.name}"/>
                         </span>
-                    </td>
-                {/each}
-            </tr>
-            <tr>
-                {#each gameSet.result as result}
-                    <td class="text-center">
+                        </td>
+                    {/each}
+                </tr>
+                <tr>
+                    {#each gameSet.result as result}
+                        <td class="text-center">
                         <span class="font-weight-bold ml-2">
                             {result.totalPoints}
                             {#if result.totalPoints > 0 && result.fundsDifference > 0}
                                 <span class="badge badge-success">+{result.fundsDifference}</span>
                             {/if}
                         </span>
-                    </td>
-                {/each}
-            </tr>
-        </table>
+                        </td>
+                    {/each}
+                </tr>
+            </table>
+        </div>
         <div class="float-right">
             <button type="button" class="btn btn-info" id="viewGame" on:click={() => dispatch('view', gameSet.games[0])}>View</button>
         </div>
